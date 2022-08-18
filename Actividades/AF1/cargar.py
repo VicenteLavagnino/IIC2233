ed94cf770dff0ec3afd4490686f179bc184b4ba4
VicenteLavagnino <vicente.lavagnino@uc.cl>
2022-08-18 15:58:52 -0400

    fix cargar.py function

--- Actividades/AF1/cargar.py
+++ Actividades/AF1/cargar.py
@@ -1,7 +1,34 @@
+from collections import namedtuple
+
 # --- EXPLICACION --- #
-# los datos vienen en este orden el el .csv:
+# los datos vienen en este orden en el .csv:
 # nombre,categoria,tiempo_preparacion,precio,ingrediente_1,...,ingrediente_n
+
+# cargar platos retorna una lista de namedtuples, donde cada namedtuple contiene información de un plato
+
 def cargar_platos(ruta_archivo: str) -> list:
+    
+    lista_notuple = []
+    lista_namedtuples = []
+
+    with open(platos.csv, 'r') as platos:
+        
+        for linea in platos:
+
+            linea = linea.strip()
+            linea = linea.split(',')
+            linea[4] = linea[4].split(';')
+
+            lista_notuple.append(linea)
+
+    mi_tupla = namedtuple("tupla",["nombre", "categoria", "tiempo_preparacion", "precio", "ingredientes"])
+
+    #convertir lista de listas a lista de namedtuples
+
+    for i in range(len(lista_notuple)):
+
+        lista_namedtuples.append(mi_tupla(lista_notuple[0],lista_notuple[1],lista_notuple[2],lista_notuple[3],lista_notuple[4]))
+
     pass
 
 
