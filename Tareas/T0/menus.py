704f04578639b7eb00b469b4010296feb2dd33c9
VicenteLavagnino <vicente.lavagnino@uc.cl>
2022-08-18 15:06:24 -0400

    added the AF1 inicial commits

---
+++ Tareas/T0/menus.py
@@ -0,0 +1,19 @@
+#in this file the menus are defined
+
+def Menu_de_inicio():
+
+    print("Bienvenido a Star Advanced/n")
+    
+    print("[0] Salir")
+    print("[1] Partida nueva")
+    print("[2] Cargar una partida")
+    print("[3] Ranking de puntajes")
+
+    eleccion = int(input("Ingrese una opción: "))
+
+    return eleccion + 1
+
+
+
+
+        
