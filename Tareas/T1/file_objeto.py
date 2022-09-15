be06e82525e36531556d381e526ed744b13ff82d
VicenteLavagnino <vicente.lavagnino@uc.cl>
2022-09-15 19:06:44 -0300

    Avance T1

--- Tareas/T1/file_objeto.py
+++ Tareas/T1/file_objeto.py
@@ -1,5 +1,17 @@
-from random import randint
-from parametros import PROB_EXITO_BAYA, PROB_EXITO_CARAMELO, PROB_EXITO_POCION
+from random import randint, random
+from parametros import (
+    PROB_EXITO_BAYA,
+    PROB_EXITO_CARAMELO,
+    PROB_EXITO_POCION,
+    AUMENTAR_ATAQUE_FUEGO,
+    AUMENTO_DEFENSA,
+    AUMENTAR_VIDA_PLANTA,
+    AUMENTAR_VELOCIDAD_AGUA,
+    PROB_EXITO_OBJETO,
+    PROB_EXITO_BAYA,
+    PROB_EXITO_POCION,
+    PROB_EXITO_CARAMELO,
+)
 
 
 class Objeto:
@@ -36,10 +48,64 @@ class Objeto:
 
     def aplicar_objeto(self, entrenador, programon):
 
+        programon.vida += self.aumento_vida
+        programon.ataque += AUMENTAR_ATAQUE_FUEGO
+        programon.defensa += AUMENTO_DEFENSA
+        programon.vida += self.aumento_vida
+
+        if programon.tipo == "Planta":
+            programon.vida += AUMENTAR_VIDA_PLANTA
+
         print(f"Programon beneficiado {programon.nombre}")
         print(f"Objeto utilizado {self.nombre} de tipo {self.tipo}")
-
-        print(f"Aumento de vida")
+        print(f"Aumento de vida {self.aumento_vida}")
+        print(
+            f"La vida subió de {programon.vida - self.aumento_vida} a {programon.vida}"
+        )
+        print(f"Aumento de ataque {AUMENTAR_ATAQUE_FUEGO}")
+        print(
+            f"El ataque subió de {programon.ataque - AUMENTAR_ATAQUE_FUEGO} a {programon.ataque}"
+        )
+        print(f"Aumento de defensa {AUMENTO_DEFENSA}")
+        print(
+            f"La defensa subió de {programon.defensa - AUMENTO_DEFENSA} a {programon.defensa}"
+        )
+        print(f"Aumento de velocidad {AUMENTAR_VELOCIDAD_AGUA}")
+        print(f"La velocidad subió de {programon.velocidad} a {programon.velocidad}")
         pass
 
     pass
+
+    def crear_objeto(self, entrenador, tipo):
+
+        if tipo == "Baya":
+
+            if random() > PROB_EXITO_BAYA:
+                print("No se pudo crear el objeto")
+
+            else:
+                objeto = Objeto("Baya", "Baya")
+                print("Se creó el objeto")
+                entrenador.objetos.append(objeto)
+
+        elif tipo == "Pocion":
+
+            if random() > PROB_EXITO_POCION:
+                print("No se pudo crear el objeto")
+
+            else:
+                objeto = Objeto("Pocion", "Pocion")
+                print("Se creó el objeto")
+                entrenador.objetos.append(objeto)
+
+        elif tipo == "Caramelo":
+
+            if random() > PROB_EXITO_CARAMELO:
+                print("No se pudo crear el objeto")
+
+            else:
+                objeto = Objeto("Caramelo", "Caramelo")
+                print("Se creó el objeto")
+                entrenador.objetos.append(objeto)
+
+        return objeto
