d423b06c44860d4e7a66b87579a96ad1ba78b698
VicenteLavagnino <vicente.lavagnino@uc.cl>
2022-09-29 16:47:31 -0300

    Avance AS3

--- Actividades/AS3/main.py
+++ Actividades/AS3/main.py
@@ -2,10 +2,12 @@ import sys
 
 from dccubitos import DCCubitos
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+
     def hook(type_, value, traceback):
         print(type_)
         print(traceback)
+
     sys.__excepthook__ = hook
 
     juego = DCCubitos(sys.argv)
