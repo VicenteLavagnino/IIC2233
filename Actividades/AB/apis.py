08aab14aee95d1625c1b9e86b6a0cf161095a483
VicenteLavagnino <vicente.lavagnino@uc.cl>
2022-11-18 11:59:25 -0300

    Avance Actividad Bonus

--- Actividades/AB/apis.py
+++ Actividades/AB/apis.py
@@ -6,12 +6,31 @@ from parametros import GITHUB_BASE_URL, GITHUB_REPO_OWNER, GITHUB_REPO_NAME
 from parametros import GITHUB_USERNAME
 from parametros import ANIME_BASE_URL, ANIME_NUMERO
 from utils.anime import Anime
+from collections import namedtuple
 
 
 def get_animes() -> Tuple[int, List[Anime]]:
     # ToDo: Completar
     status_code = 404
     animes = []
+
+    # url = https://backend.chan.ing.puc.cl/animeranking/v1/AB?id=2
+
+    url = ANIME_BASE_URL.format("AB?id=" + ANIME_NUMERO)
+    response = requests.get(url)
+    animes_received = response.json()["animes"]
+
+    status_code = response.status_code
+
+    for anime in animes_received:
+
+        nuevo = Anime(anime["name"], anime["season"]["year"], anime["tags"])
+        animes.append(nuevo)
+
+    """for anime in animes:
+        print(anime.nombre)"""
+
+    # print(status_code)
 
     return status_code, animes
 
@@ -21,12 +40,48 @@ def post_issue(token, animes: List[Anime]) -> Tuple[int, int]:
     status_code = 404
     issue_number = -1
 
+    # codigo inspirado en https://docs.github.com/en/rest/issues/issues#create-an-issue
+    # url = https://api.github.com/repos/IIC2233/VicenteLavagnino-iic2233-2022-2/issues
+    url = GITHUB_BASE_URL.format(GITHUB_REPO_OWNER + "/" + GITHUB_REPO_NAME + "/issues")
+
+    headers = {
+        "Authorization": "Bearer " + token,
+        "Accept": "application/vnd.github+json",
+    }
+
+    data = {
+        "owner": GITHUB_USERNAME,
+        "repo": GITHUB_REPO_NAME,
+        "title": GITHUB_USERNAME,
+        "body": str(animes),
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+
+    status_code = response.status_code
+    issue_number = response.json()["number"]
+
     return status_code, issue_number
 
 
 def put_lock_issue(token: str, numero_issue: int) -> int:
     # ToDo: Completar
     status_code = 404
+
+    # codigo inspirado en https://docs.github.com/en/rest/issues/issues#lock-an-issue
+    # url = https://api.github.com/repos/IIC2233/VicenteLavagnino-iic2233-2022-2/issues/numero_issue
+    url = GITHUB_BASE_URL.format(
+        GITHUB_REPO_OWNER + "/" + GITHUB_REPO_NAME + "/issues" + f"/{numero_issue}/lock"
+    )
+
+    headers = {
+        "Authorization": "Bearer " + token,
+        "Accept": "application/vnd.github+json",
+        "lock_reason": "off-topic",
+    }
+
+    response = requests.put(url, headers=headers)
+    status_code = response.status_code
 
     return status_code
 
@@ -35,4 +90,22 @@ def delete_lock_issue(token: str, numero_issue: int) -> int:
     # ToDo: Completar
     status_code = 404
 
+    # codigo inspirado en https://docs.github.com/en/rest/issues/issues#unlock-an-issue
+    # url = https://api.github.com/repos/IIC2233/VicenteLavagnino-iic2233-2022-2/issues/numero_issue
+    url = GITHUB_BASE_URL.format(
+        GITHUB_REPO_OWNER + "/" + GITHUB_REPO_NAME + "/issues" + f"/{numero_issue}/lock"
+    )
+
+    headers = {
+        "Authorization": "Bearer " + token,
+        "Accept": "application/vnd.github+json",
+        "lock_reason": "off-topic",
+    }
+
+    response = requests.delete(url, headers=headers)
+
     return status_code
+
+
+if __name__ == "__main__":
+    get_animes()
--- Actividades/AB/parametros.py
+++ Actividades/AB/parametros.py
@@ -1,13 +1,18 @@
 import os.path
 
-TOKENS_FILE_PATH = os.path.join('data', 'tokens.csv')
-ISSUES_FILE_PATH = os.path.join('data', 'issues.csv')
+TOKENS_FILE_PATH = os.path.join("data", "tokens.csv")
+ISSUES_FILE_PATH = os.path.join("data", "issues.csv")
 
-ANIME_BASE_URL = 'https://backend.chan.ing.puc.cl/animeranking/v1/{}'
-ANIME_NUMERO = -1  # Completar
-REGEX_FILTRO = r'.*'  # Completar
+ANIME_BASE_URL = "https://backend.chan.ing.puc.cl/animeranking/v1/{}"
 
-GITHUB_BASE_URL = 'https://api.github.com/repos/{}'
-GITHUB_REPO_OWNER = 'IIC2233'
-GITHUB_REPO_NAME = ''  # Completar
-GITHUB_USERNAME = ''  # Completar
+# NUMERO_ALUMNO = "21638322"
+# NUMERO_ALUMNO[5:6] = 32 --> % 6 = 2
+
+ANIME_NUMERO = str(2)  # Completar
+
+REGEX_FILTRO = r"((ha)(\w)+o)|((a.*){3,})"  # Completar
+
+GITHUB_BASE_URL = "https://api.github.com/repos/{}"
+GITHUB_REPO_OWNER = "IIC2233"
+GITHUB_REPO_NAME = "VicenteLavagnino-iic2233-2022-2"  # Completar
+GITHUB_USERNAME = "VicenteLavagnino"  # Completar
